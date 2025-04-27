920a2fd2162afc675ef1dfb9535d0b108fcfcc9b
Chukwu Rophi <chukwurophi@gmail.com>
2025-04-27 18:12:15 +0100

    just learnt about event emmitters

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import { emmitterInstance } from "./eventEmmitter";
+import { loggerFn } from "./loggerFn";
+import path from "path";
+import fs from 'fs/promises';
 
 const app = express();
 configDotenv();
 
 const port = process.env.PORT;
+
+app.get('/tester', async (req, res) => {
+    try{
+        const data = await fs.readFile(path.join(__dirname, 'mockFiles', 'fake2.txt'), 'utf-8');
+        res.send('Read complete');
+    }catch(error){
+        console.log(error);
+        res.send('An error occured');
+        emmitterInstance.emit('error', error);
+        //throw new Error('An error occured during reading');
+    }  
+});
 
 app.get('/checker', (req, res)=>{
     res.send('I see youuuu 👀');
@@ -14,3 +30,7 @@ app.listen(port, ()=>{
     console.log(`Listening on port ${port} 🚀`);
 });
 
+emmitterInstance.on('error', (message)=>{
+    console.log('Error occured');
+    loggerFn(message);
+});
